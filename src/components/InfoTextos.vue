61ea8e7faffcdbdb15df8b613cc1a03924a77056
Luis Vega <70670285+070809-vega@users.noreply.github.com>
2023-04-21 13:55:35 -0600

    se corrigen errores

--- src/components/InfoTextos.vue
+++ src/components/InfoTextos.vue
@@ -1,58 +1,72 @@
 <template>
   <div>
-    <v-row no-gutters>
-      <v-col cols="12" md="4">
-        <v-card color="#EEEEEE" class="d-flex align-center flex-column" elevation="0">
-          <v-img
-            class="d-flex align-center"
-            lazy-src="@/assets/imgs/anillos.svg"
-            src="@/assets/imgs/anillos.svg"
-            width="65px"
-            height="65px"
-          ></v-img>
-          <v-container>
-            <p class="body mb-0 text-center">
-              Abitur assueverit no. Iudico putent apeirian an mei, no tritani
-              ullamcorper varius mauris eu.
-            </p>
-          </v-container>
-        </v-card>
-      </v-col>
-      <v-col cols="12" md="4">
-        <v-card color="#EEEEEE" class="d-flex align-center flex-column" elevation="0">
-          <v-img
-            class="d-flex align-center"
-            lazy-src="@/assets/imgs/corazon.svg"
-            src="@/assets/imgs/corazon.svg"
-            width="50px"
-            height="37px"
-          ></v-img>
-          <v-container>
-            <p class="body mb-0 text-center">
-              Abitur assueverit no. Iudico putent apeirian an mei, no tritani
-              ullamcorper varius mauris eu.
-            </p>
-          </v-container>
-        </v-card>
-      </v-col>
-      <v-col cols="12" md="4">
-        <v-card color="#EEEEEE" class="d-flex align-center flex-column" elevation="0">
-          <v-img
-            class="d-flex align-center"
-            lazy-src="@/assets/imgs/foto.svg"
-            src="@/assets/imgs/foto.svg"
-            width="49px"
-            height="35px"
-          ></v-img>
-          <v-container>
-            <p class="body mb-0 text-center">
-              Abitur assueverit no. Iudico putent apeirian an mei, no tritani
-              ullamcorper varius mauris eu.
-            </p>
-          </v-container>
-        </v-card>
-      </v-col>
-    </v-row>
+    <v-card color="#EEEEEE" width="100%" class="">
+      <v-row no-gutters>
+        <v-col cols="12" md="4">
+          <v-card
+            color="#EEEEEE"
+            class="d-flex align-center flex-column"
+            elevation="0"
+          >
+            <v-img
+              class="d-flex align-center"
+              lazy-src="@/assets/imgs/anillos.svg"
+              src="@/assets/imgs/anillos.svg"
+              width="65px"
+              height="65px"
+            ></v-img>
+            <v-container>
+              <p class="body mb-0 text-center">
+                Abitur assueverit no. Iudico putent apeirian an mei, no tritani
+                ullamcorper varius mauris eu.
+              </p>
+            </v-container>
+          </v-card>
+        </v-col>
+        <v-col cols="12" md="4">
+          <v-card
+            color="#EEEEEE"
+            class="d-flex align-center flex-column mt-7"
+            elevation="0"
+          >
+            <v-img
+              class="d-flex align-center"
+              lazy-src="@/assets/imgs/corazon.svg"
+              src="@/assets/imgs/corazon.svg"
+              width="50px"
+              height="37px"
+            ></v-img>
+            <v-container>
+              <p class="body mb-0 text-center">
+                Abitur assueverit no. Iudico putent apeirian an mei, no tritani
+                ullamcorper varius mauris eu.
+              </p>
+            </v-container>
+          </v-card>
+        </v-col>
+        <v-col cols="12" md="4">
+          <v-card
+            color="#EEEEEE"
+            class="d-flex align-center flex-column mt-7"
+            elevation="0"
+          >
+            <v-img
+              class="d-flex align-center"
+              lazy-src="@/assets/imgs/foto.svg"
+              src="@/assets/imgs/foto.svg"
+              width="49px"
+              height="35px"
+            ></v-img>
+            <v-container>
+              <p class="body mb-0 text-center">
+                Abitur assueverit no. Iudico putent apeirian an mei, no tritani
+                ullamcorper varius mauris eu.
+              </p>
+            </v-container>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-card>
   </div>
 </template>
 
